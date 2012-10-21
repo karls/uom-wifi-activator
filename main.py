ef19e110674443202fceec589530b1d8f05185f0
Karl Sutt <karl@sutt.ee>
2012-10-21 14:52:35 +0100

    Logbook is now optional.

--- main.py
+++ main.py
@@ -7,25 +7,51 @@ from urllib import urlencode
 from urllib2 import urlopen, Request
 from urlparse import urlparse, urlunparse
 
-from logbook import Logger, RotatingFileHandler
+_has_logbook = False
+try:
+    from logbook import Logger, RotatingFileHandler
+    _has_logbook = True
+except ImportError:
+    pass
 
 from credentials import USERNAME, PASSWORD
 
+_logger = None
+
+def setup_logger():
+    """
+    Install a logger into the global namespace
+    """
+
+    global _logger
+
+    if _has_logbook:
+        _logger = Logger('UoM_WIFI')
+        try:
+            log_path = join(sys.argv[1], '%s.log' % USERNAME)
+        except IndexError:
+            log_path = join(split(abspath(__file__))[0], '%s.log' % USERNAME)
+
+        # create the handler
+        log_handler = RotatingFileHandler(log_path)
+        # push the context object to the application stack
+        log_handler.push_application()
+
+
+def log(msg):
+    """
+    A small wrapper around the logger object
+    """
+
+    if _has_logbook:
+        _logger.info(msg)
 
 
 def main():
-    logger = Logger('Log')
-    try:
-        log_path = join(sys.argv[1], '%s.log' % USERNAME)
-    except IndexError:
-        log_path = join(split(abspath(__file__))[0], '%s.log' % USERNAME)
 
-    # create the handler
-    log_handler = RotatingFileHandler(log_path)
-    # push the context object to the application stack
-    log_handler.push_application()
+    setup_logger()
 
-    logger.info('NetworkManager has connected to UoM_WIFI.')
+    log('NetworkManager has connected to UoM_WIFI.')
 
     # POST to
     FORM_ACTION = '/auth/index.html/u'
@@ -41,22 +67,22 @@ def main():
     }
 
     # try to navigate to Google
-    logger.info('Opening test URL %s.' % TEST_URL)
+    log('Opening test URL %s.' % TEST_URL)
     page = urlopen(TEST_URL)
     url = urlparse(page.url)
 
     # if we're  on *.google.* domain, it's all good and we carry on browsin'
     if 'google' in url.netloc:
-        logger.info('Looks like %s is authenticated, bye-bye.' % USERNAME)
+        log('Looks like %s is authenticated, bye-bye.' % USERNAME)
         sys.exit(0)
 
     # otherwise, let's build the URL to POST things to and add other necessary
     # information
-    logger.info('Looks like %s is not authenticated.' % USERNAME)
-    logger.info('Build the authentication URL.')
+    log('Looks like %s is not authenticated.' % USERNAME)
+    log('Build the authentication URL.')
     POST_URL = urlunparse((url.scheme, url.netloc, FORM_ACTION, '', '', ''))
 
-    logger.info('Building the request object.')
+    log('Building the request object.')
     request = Request(POST_URL, urlencode(FORM_DATA))
     request.add_header('Accept',
                        'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
@@ -70,7 +96,7 @@ def main():
     # this call to urlopen POSTs the urlencoded FORM_DATA, along with the specified
     # headers to POST_URL
     # TODO: add some sensible verification, error checking and fallbacks
-    logger.info('Trying to authenticate.')
+    log('Trying to authenticate.')
     auth_page = urlopen(request)
     sys.exit(0)
 
